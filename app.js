b87997b41428c7f0bec5c421a2dd01317b6468de
Taioh Kubota <kubotataioh@gmail.com>
2023-05-04 14:27:15 -0400

    created basic input field and css styling

---
+++ app.js
@@ -0,0 +1,7 @@
+const inputField = document.getElementById("input-field")
+const addButton = document.getElementById("add-button")
+
+addButton.addEventListener("click", ()=>{
+    let inputVal = inputField.value
+    console.log(inputVal)
+})
