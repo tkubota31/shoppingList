212abe83cbce585adb8ef9cc37bfc064feae0146
Taioh Kubota <kubotataioh@gmail.com>
2023-05-04 16:02:44 -0400

    implement firebase database

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings ={
+    databaseURL: "https://playground-d5878-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListDB = ref(database, "shoppingList")
+
+
 const inputField = document.getElementById("input-field")
 const addButton = document.getElementById("add-button")
 
 addButton.addEventListener("click", ()=>{
     let inputVal = inputField.value
-    console.log(inputVal)
+    push(shoppingListDB, inputVal)
 })
